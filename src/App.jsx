319d2fab2d799d1cfdb2af932a86bbe11bd503de
Reid Vanvliet <Reidvanvliet56@hotmail.com>
2025-04-30 16:57:47 -0700

    layed out the html for rendering

--- src/App.jsx
+++ src/App.jsx
@@ -3,31 +3,49 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  //const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header>
+        <h1>Jammming</h1>
+      </header>
+      <body>
+        <div>
+          <h2>Add Your Favorite Spotify Songs!</h2>
+        </div>
+
+        //probably import this with a searchBar component
+        <div className="search">
+          <img className='searchIcon'/>
+          <select name="params" id="params">
+            <option value="song" selected>Song</option>
+            <option value="genre" selected>Genre</option>
+            <option value="artist" selected>Artist</option>
+            <option value="album" selected>Album</option>
+          </select>
+        </div>
+
+        <div className='results'>
+          //these are added with our searchResults component
+          <img className="albumCover"/>
+          <p>{songname}</p>
+          <p>{artist}</p>
+          <button className="add">+</button>
+        </div>
+
+        <div className="playlist">
+          <h3>{playlistTitle}</h3>
+          <button className="editTitle"><img id="littlePencil"/></button>
+          //these are added with our AddSong component
+          <p>{songname}</p>
+          <p>{artist}</p>
+          <button className="delete">-</button>
+          <button className="saveSpotify">Save to Spotify</button>
+        </div>
+      </body>
     </>
   )
 }
